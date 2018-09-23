9bd6e005b25fbe9572f5736541e9dd33e54aa32b
jason.huang22 <jason.huang@titansoft.com.sg>
2018-09-23 23:49:11 +0800

    complete the test

---
+++ VowelCount/VowelCount/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace VowelCount
+{
+    public class Kata
+    {
+        public int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            char[] vowels = {'a','e','i','o','u' };
+            foreach (var vowel in vowels)
+            {
+                vowelCount += str.Count(x => x.Equals(vowel));
+            }
+            return vowelCount;
+        }
+    }
+}
---
+++ VowelCount/VowelCountTests/KataTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VowelCount.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [DataTestMethod]
+        [DataRow("abracadabra", 5, DisplayName = "result_should_return_5")]
+        [DataRow("aqqqebbbiuuuo", 7, DisplayName = "result_should_return_4")]
+        [DataRow("bcdfghlppppzzz", 0, DisplayName = "result_should_return_0")]
+        public void GetVowelCountTestWithNoSpaces(string input,int expect)
+        {
+            var actural = new Kata().GetVowelCount(input);
+            Assert.AreEqual(expect,actural);
+        }
+
+        [DataTestMethod]
+        [DataRow("  abracadabra", 5, DisplayName = "result_should_return_5")]
+        [DataRow("abrac  adabra", 5, DisplayName = "result_should_return_5")]
+        [DataRow("abracadabra  ", 5, DisplayName = "result_should_return_5")]
+        [DataRow("  abrac  adabra  ", 5, DisplayName = "result_should_return_5")]
+        [DataRow("               ", 0, DisplayName = "result_should_return_0")]
+        public void GetVowelCountTestWithSpace(string input, int expect)
+        {
+            var actural = new Kata().GetVowelCount(input);
+            Assert.AreEqual(expect, actural);
+        }
+    }
+}
